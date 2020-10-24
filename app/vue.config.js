84ae38c7b9ffd585f8dcd49e7da16393f9b70b50
Thomas Ballmann <mail@i-2xdream.de>
2020-10-24 11:24:45 +0200

    eslint config added

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,39 +1,39 @@
-const webpack = require('webpack');
-const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
-	"outputDir": "../data/dist",
-	"filenameHashing": false,
-	"productionSourceMap": false,
-	"transpileDependencies": [
-		"vuetify"
-	],
-	devServer: {
-		proxy: {
-			'^/': {
-				target: 'http://192.168.178.88:80',
-				ws: true,
-				changeOrigin: true
-			},
-		}
-	},
-	configureWebpack: () => {
-		if (process.env.NODE_ENV === 'production') {
-			// optimize build for esp
-			return {
-				plugins: [
-					// reduce total size of the app
-					new webpack.optimize.LimitChunkCountPlugin({
-						maxChunks: 1
-					}),
-					// use only pre compressed files
-					new CompressionPlugin({
-						deleteOriginalAssets: true
-					})
-				]
-			}
-		} else {
-			// mutate for development...
-		}
-	}
+  outputDir: '../data/dist',
+  filenameHashing: false,
+  productionSourceMap: false,
+  transpileDependencies: [
+    'vuetify',
+  ],
+  devServer: {
+    proxy: {
+      '^/': {
+        target: 'http://paperdash-weatherstation:80',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      // optimize build for esp
+      return {
+        plugins: [
+          // reduce total size of the app
+          new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1,
+          }),
+          // use only pre compressed files
+          new CompressionPlugin({
+            deleteOriginalAssets: true,
+          }),
+        ],
+      }
+    } else {
+      // mutate for development...
+    }
+  },
 }
